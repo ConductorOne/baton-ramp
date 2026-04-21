d13344e8a490e91007b303e772f8a36f6fb859e7
Alejandro Bernal <alejandro.bernal@conductorone.com>
2026-04-21 09:54:44 -0500

    CXH-1376: annotate resource types with required API scopes

--- pkg/connector/resource_types.go
+++ pkg/connector/resource_types.go
@@ -2,17 +2,33 @@ package connector
 
 import (
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
+	"github.com/conductorone/baton-sdk/pkg/annotations"
 )
+
+func capabilityPermissions(perms ...string) *v2.CapabilityPermissions {
+	cp := &v2.CapabilityPermissions{}
+	for _, p := range perms {
+		cp.Permissions = append(cp.Permissions, &v2.CapabilityPermission{Permission: p})
+	}
+	return cp
+}
 
 // The user resource type is for all user objects from the database.
 var userResourceType = &v2.ResourceType{
 	Id:          "user",
 	DisplayName: "User",
 	Traits:      []v2.ResourceType_Trait{v2.ResourceType_TRAIT_USER},
+	Annotations: annotations.New(capabilityPermissions(
+		"users:read",
+		"users:write",
+	)),
 }
 
 var roleResourceType = &v2.ResourceType{
 	Id:          "role",
 	DisplayName: "Role",
 	Traits:      []v2.ResourceType_Trait{v2.ResourceType_TRAIT_ROLE},
+	Annotations: annotations.New(capabilityPermissions(
+		"users:read",
+	)),
 }
